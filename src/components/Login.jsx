4e71273f1dbd27b786231e67696e96d3c0cf3e4a
Avinash <avinashkumarofficial95@gmail.com>
2025-04-22 18:07:10 +0530

    made signin/login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
-
+import Header from './Header'
 function Login() {
   return (
-    <div>Login</div>
+    <div className='h-screen relative'>
+        <Header/>
+        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/69bec183-9cc8-49d4-8fc2-08228d3c91b4/web/IN-en-20250414-TRIFECTA-perspective_c8273fb1-8860-4ff5-bd1c-c2c4b44d5f2a_medium.jpg" alt="" />
+        <form action="" className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+        bg-gray-600  px-8 py-2 w-full max-w-md text-center '>
+            <h1 className='text-left text-white font-bold mb-8 text-4xl'>Sign In</h1>
+            <div className='space-y-4'>
+                <div>
+                    <input type="text" name="" id="" placeholder='Email or mobile number'
+                    className='w-full bg-zinc-800 border border-zinc-700 text-white p-4 rounded focus:outline-none
+                    focus:border-zinc-500'/>
+                </div>
+                <div>
+                    <input type="password" name="" id="" placeholder='Password'
+                    className='w-full bg-zinc-800 border border-zinc-700 text-white p-4 focus:outline-none
+                    focus:border-zinc-500'/>
+                </div>
+                <div>
+                    <button className='w-full bg-red-600 text-white py-3 rounded font-medium hover:bg-red-700 transition-colors'>Sign In</button>
+                </div>
+                <div>
+                    <span className='text-zinc-400'>OR</span>
+                </div>
+                <div>
+                    <button className='w-full bg-zinc-700 text-white py-3 rounded font-medium hover:bg-zinc-600 transition-colors'>Use a sign-in code</button>
+                </div>
+                <div>
+                    <a href="#" className='text-white hover:underline'>Forgot password?</a>
+                </div>
+                <div>
+                    <p className='text-zinc-400'>New to Netflix?
+                        <a href="#" className='text-white ml-1 hover:underline'>Sign up Now</a>
+                    </p>
+                </div>
+                <div className='mt-6 text-xs text-zinc-500'>
+                    <p>This page is protected by Google reCAPTCHA to ensure you're not a bot.</p>
+                    <a href="#" className='text-blue-500 hover:underline'>Learn more</a>
+                </div>
+            </div>
+        </form>
+    </div>
+   
   )
 }
 
