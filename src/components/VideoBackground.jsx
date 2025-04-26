0bae11637e7dc18f55b10ed438a3ac66196cd9c9
Avinash <avinashkumarofficial95@gmail.com>
2025-04-26 16:55:44 +0530

    added youtube video inside my main container

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,18 +1,33 @@
 import React, { useEffect } from 'react'
 import { options } from '../utils/constant'
+
 function VideoBackground({movieId}){
 
     const getMovieTrailer = async()=>{
         const data = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`, options);
         const json = await data.json()
-        console.log('movieTrailer',json);
+        console.log('videos',json.results);
+        const filteredMovie = json.results.filter(video=>video.type==='Trailer');
+        const movieTrailer = filteredMovie.length ? filteredMovie[0] : json.results[0];
+        console.log('movietrailer',movieTrailer);
+        
         
     }
     useEffect(()=>{
         getMovieTrailer();
     },[])
   return (
-    <div>VideoBackground</div>
+    <div className='p-3'>
+      <iframe 
+      width="560" 
+      height="315" 
+      src="https://www.youtube.com/embed/mdfrG2cLK58?si=fRqgbzwYcurVZD6R" 
+      title="YouTube video player" 
+      frameBorder="0" 
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+      referrerPolicy="strict-origin-when-cross-origin" 
+      allowFullScreen></iframe>
+    </div>
   )
 }
 
