fe9a832d471b6514a9252dd85e491da0bcb4636f
Avinash <avinashkumarofficial95@gmail.com>
2025-04-26 17:26:40 +0530

    made the youtube key dynamic

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect } from 'react'
 import { options } from '../utils/constant'
-
+import {addMovieTrailer} from "./../utils/movieSlice.js"
+import { useDispatch, useSelector } from 'react-redux';
 function VideoBackground({movieId}){
+    const dispatch = useDispatch();
+    const movieTrailer = useSelector(store=>store.movies?.movieTrailer);
 
     const getMovieTrailer = async()=>{
         const data = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`, options);
@@ -10,7 +13,7 @@ function VideoBackground({movieId}){
         const filteredMovie = json.results.filter(video=>video.type==='Trailer');
         const movieTrailer = filteredMovie.length ? filteredMovie[0] : json.results[0];
         console.log('movietrailer',movieTrailer);
-        
+        dispatch(addMovieTrailer(movieTrailer))
         
     }
     useEffect(()=>{
@@ -18,15 +21,7 @@ function VideoBackground({movieId}){
     },[])
   return (
     <div className='p-3'>
-      <iframe 
-      width="560" 
-      height="315" 
-      src="https://www.youtube.com/embed/mdfrG2cLK58?si=fRqgbzwYcurVZD6R" 
-      title="YouTube video player" 
-      frameBorder="0" 
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-      referrerPolicy="strict-origin-when-cross-origin" 
-      allowFullScreen></iframe>
+      <iframe width="560" height="315" src={`https://www.youtube.com/embed/${movieTrailer?.key}?si=fRqgbzwYcurVZD6R`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
   )
 }
