211ed8c805c0a2e35e83c7138cdf694c9e2cfe8b
Avinash <avinashkumarofficial95@gmail.com>
2025-04-26 19:43:19 +0530

    separated video fetching of a movie and dispatch action to a hook

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,27 +1,15 @@
-import React, { useEffect } from 'react'
-import { options } from '../utils/constant'
-import {addMovieTrailer} from "./../utils/movieSlice.js"
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react'
+import useMovieTrailer from './hooks/useMovieTrailer';
+import { useSelector } from 'react-redux';
 function VideoBackground({movieId}){
-    const dispatch = useDispatch();
-    const movieTrailer = useSelector(store=>store.movies?.movieTrailer);
-
-    const getMovieTrailer = async()=>{
-        const data = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`, options);
-        const json = await data.json()
-        console.log('videos',json.results);
-        const filteredMovie = json.results.filter(video=>video.type==='Trailer');
-        const movieTrailer = filteredMovie.length ? filteredMovie[0] : json.results[0];
-        console.log('movietrailer',movieTrailer);
-        dispatch(addMovieTrailer(movieTrailer))
-        
-    }
-    useEffect(()=>{
-        getMovieTrailer();
-    },[])
+ 
+  const videoTrailer = useSelector(store=>store.movies?.movieTrailer);
+     
+  useMovieTrailer(movieId);
+    
   return (
     <div className='p-3'>
-      <iframe width="560" height="315" src={`https://www.youtube.com/embed/${movieTrailer?.key}?si=fRqgbzwYcurVZD6R`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <iframe width="560" height="315" src={`https://www.youtube.com/embed/${videoTrailer?.key}?si=fRqgbzwYcurVZD6R`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"  ></iframe>
     </div>
   )
 }
---
+++ src/components/hooks/useMovieTrailer.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { options } from "../../utils/constant";
+import { addMovieTrailer } from "../../utils/movieSlice";
+
+const useMovieTrailer = (movieId) =>{
+    const dispatch = useDispatch();
+    const getMovieTrailer = async()=>{
+        const data = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`, options);
+        const json = await data.json()
+        // console.log('videos',json.results);
+        const filteredMovie = json.results.filter(video=>video.type==='Trailer');
+        const movieTrailer = filteredMovie.length ? filteredMovie[0] : json.results[0];
+        console.log('movietrailer',movieTrailer);
+        dispatch(addMovieTrailer(movieTrailer))
+        
+
+    }
+    useEffect(()=>{
+        getMovieTrailer();
+    },[])
+}
+
+export default useMovieTrailer
